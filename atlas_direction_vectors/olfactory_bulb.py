bdf5e5b1c0a8c11dda70a259414fd467f1c950f8
arnaudon <alexis.arnaudon@epfl.ch>
2024-02-01 11:18:31 +0100

    Feat: Add olfactory bulb

---
+++ atlas_direction_vectors/olfactory_bulb.py
@@ -0,0 +1,80 @@
+"""
+Function computing the direction vectors of the mouse olfactory bulb
+"""
+from __future__ import annotations
+
+import logging
+
+import numpy as np
+from atlas_commons.typing import NDArray
+from atlas_commons.utils import get_region_mask
+from voxcell import RegionMap, VoxelData  # type: ignore
+
+from atlas_direction_vectors.algorithms import layer_based_direction_vectors
+from atlas_direction_vectors.utils import warn_on_nan_vectors
+
+L = logging.getLogger(__name__)
+logging.captureWarnings(True)
+
+
+def compute_direction_vectors(region_map: RegionMap, annotation: VoxelData) -> NDArray[np.float32]:
+    """Returns the direction vectors computed
+
+    Arguments:
+        region_map: RegionMap object with the hierarchy data
+        annotation: VoxelData object containing the isocortex or a superset.
+
+    Returns:
+        numpy.ndarray of shape (annotation.shape, 3) holding a 3D unit vector field.
+    """
+
+    metadata = {
+        "region": {
+            "name": "Main olfactory bulb",
+            "query": "@^\\bMOB|lfbs\\b$",
+            "attribute": "acronym",
+            "with_descendants": True,
+        },
+        "layers": {
+            "names": [
+                "glomerular",
+                "granule",
+                "inner_plexiform",
+                "mitral",
+                "outer_plexiform",
+            ],
+            "queries": [
+                "MOBgl",
+                "MOBgr",
+                "MOBipl",
+                "MOBmi",
+                "MOBopl",
+            ],
+            "attribute": "acronym",
+            "with_descendants": True,
+        },
+    }
+    region_to_weight = {
+        "MOBgl": 5,
+        "MOBgr": 4,
+        "MOBipl": 3,
+        "MOBmi": 2,
+        "MOBopl": 1,
+        "outside_of_brain": 0,
+    }
+
+    direction_vectors = layer_based_direction_vectors.compute_layered_region_direction_vectors(
+        region_map=region_map,
+        annotation=annotation,
+        metadata=metadata,
+        region_to_weight=region_to_weight,
+        shading_width=4,
+        expansion_width=8,
+        has_hemispheres=True,
+    )
+
+    warn_on_nan_vectors(
+        direction_vectors, get_region_mask("MOB", annotation.raw, region_map), "MOB"
+    )
+
+    return direction_vectors
